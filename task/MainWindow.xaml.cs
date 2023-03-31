947b111b984d1b2b82a4d619f44a676b061b191d
blazhkevych.v <blazhkevych.v@gmail.com>
2023-03-31 14:20:58 +0300

    максиму 3 копии - готово

--- task/MainWindow.xaml.cs
+++ task/MainWindow.xaml.cs
@@ -10,10 +10,27 @@ namespace task;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private static Semaphore? mainAppSemaphore;
+    private const int MAX_COPIES = 3;
+    private string[] Guids = new string[MAX_COPIES] { "{84079a08-eb1c-4045-941e-08a5f337d471}", "{84079a08-eb1c-4045-941e-08a5f337d472}", "{84079a08-eb1c-4045-941e-08a5f337d473}" };
     public MainWindow()
     {
+        bool createdNew;
+        for (int i = 0; i < MAX_COPIES; i++)
+        {
+            mainAppSemaphore = new Semaphore(MAX_COPIES, MAX_COPIES, Guids[i], out createdNew);
+            if (!createdNew)
+            {
+                if (i == MAX_COPIES - 1)
+                {
+                    Close();
+                    return;
+                }
+            }
+            else
+                break;
+        }
+
         InitializeComponent();
-        
     }
-    
 }
